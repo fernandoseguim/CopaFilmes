4b69da48f30415c474b5824014ab9d4ebaa0202e
Fernando Seguim <fernando.seguim@gmail.com>
2018-08-30 19:52:59 -0300

    initial commit

---
+++ src/CopaFilmes.Api/Domain/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace CopaFilmes.Api.Domain
+{
+	public class Movie
+    {
+	    public int Id  { get; set; }
+	    public string Title { get; set; }
+	    public int ReleaseYear { get; set; }
+	    public int Score { get; set; }
+	}
+}
---
+++ src/CopaFilmes.Api/ExternalServices/MoviesService.cs
@@ -0,0 +1,16 @@
+﻿using CopaFilmes.Api.Domain;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace CopaFilmes.Api.ExternalServices
+{
+	public class MoviesService : IMoviesService
+    {
+	    public IEnumerable<Movie> GetMovies()
+	    {
+			var client = new HttpClient();
+			
+		    return default(IEnumerable<Movie>);
+	    }
+    }
+}
